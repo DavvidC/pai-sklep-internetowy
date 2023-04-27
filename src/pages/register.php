abd29c17debcb89b793fe53d1feed0feea1ad66d
Michal Cieslik <michalcieslik105@gmail.com>
2023-04-27 16:07:51 +0200

    Add login/register PHP code, reorganize app structure

---
+++ src/pages/register.php
@@ -0,0 +1,18 @@
+<?php require_once("/templates/header-http.php"); ?>
+    <h1>Register Page</h1>
+    <form method="POST" action="../php/process_register.php">
+      <label for="email">Email:</label>
+      <input type="text" id="email" name="email" />
+      <br />
+      <label for="username">Username:</label>
+      <input type="text" id="username" name="username" />
+      <br />
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password" />
+      <br />
+      <div class="buttons">
+        <input type="submit" value="Sign up" />
+      </div>
+    </form>
+  </body>
+</html>
